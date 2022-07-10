63a418e1d4d4afc51497615d96f22f8552d8056f
sushant <sushantchouhan90@gmail.com>
2022-07-10 19:38:45 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import {Button} from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='container'>
+        <h1>Welcome to React JS</h1>
+        <Button className='btn-success'>Bootstrap</Button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
